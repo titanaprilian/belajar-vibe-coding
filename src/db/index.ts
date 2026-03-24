6c9549c65978634f97b3e8efda40d02e23ec4ab3
titanaprilian <titanaprilian73@gmail.com>
2026-03-24 13:02:09 +0700

    chore: reset repo

--- src/db/index.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from 'drizzle-orm/mysql2';
-import mysql from 'mysql2/promise';
-import { env } from 'bun';
-
-const pool = mysql.createPool({
-  uri: env.DATABASE_URL,
-});
-
-export const db = drizzle(pool);
